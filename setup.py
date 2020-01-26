9e21d882c2e86f7ab58563992fc03e61e5961e90
Mustafa Mohamed <ms7mohamed@gmail.com>
2020-01-26 12:30:29 -0500

    add setup.py

---
+++ setup.py
@@ -0,0 +1,69 @@
+
+import io
+import os
+import sys
+from shutil import rmtree
+
+from setuptools import Command, find_packages, setup
+
+NAME = "pybyte"
+DESCRIPTION = "Byte API Wrapper for Python"
+AUTHOR = "Mustafa Mohamed"
+EMAIL = "mustafa@ms7m.me"
+REQUIRES_PYTHON = '>=3.6.0'
+URL = 'https://github.com/ms7m/py-byte'
+VERSION = '0.0.4'
+
+
+
+
+REQUIRED = [
+    'requests',
+    'arrow',
+    'loguru'
+]
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+# Import the README and use it as the long-description.
+# Note: this will only work if 'README.md' is present in your MANIFEST.in file!
+try:
+    with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+        long_description = '\n' + f.read()
+except FileNotFoundError:
+    long_description = DESCRIPTION
+
+
+# Where the magic happens:
+setup(
+    name=NAME,
+    version="0.0.1",
+    description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author=AUTHOR,
+    author_email=EMAIL,
+    python_requires=REQUIRES_PYTHON,
+    url=URL,
+    packages=find_packages(exclude=["testing", "*.testing", "*.testing.*", "testing.*"]),
+    # If your package is a single module, use this instead of 'packages':
+    # py_modules=['mypackage'],
+
+    # entry_points={
+    #     'console_scripts': ['mycli=mymodule:cli'],
+    # },
+    install_requires=REQUIRED,
+    include_package_data=True,
+    license='MIT',
+    classifiers=[
+        # Trove classifiers
+        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy'
+    ],
+    # $ setup.py publish support.
+)
