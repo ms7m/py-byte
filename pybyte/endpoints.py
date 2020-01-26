724ac9a3610cbadaaf394e678c88b66503805db0
Mustafa Mohamed <ms7mohamed@gmail.com>
2020-01-26 12:43:36 -0500

    rebyte endpoint and like endpoint

--- pybyte/endpoints.py
+++ pybyte/endpoints.py
@@ -1,6 +1,8 @@
 
 
 base = "https://api.byte.co"
+def return_like(string)
+    return base + f"/post/id/{string}/feedback/like"
 
 class Endpoints:
     GOOGLE_LOGIN = base + "/authenticate/google"
@@ -8,3 +10,8 @@ class Endpoints:
     USERNAME_CHECK = base + "/"
     OTHER_ACCOUNT = base + "/account/id/"
     POST_INFO = base + "/post/id/"
+    REBYTE = base + "/rebyte"
+
+    
+
+    LIKE = return_like
