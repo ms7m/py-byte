b3df1cc8b18e53eeb1464043b34e6a6b5bbd98bf
Mustafa Mohamed <ms7mohamed@gmail.com>
2020-01-27 19:32:00 -0500

    bump version

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ except FileNotFoundError:
 # Where the magic happens:
 setup(
     name=NAME,
-    version="0.1.1",
+    version="0.1.3",
     description=DESCRIPTION,
     long_description=long_description,
     long_description_content_type='text/markdown',
